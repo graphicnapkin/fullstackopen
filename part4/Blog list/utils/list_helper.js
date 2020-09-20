996701201b90f5ba654984b973bf2de542f8fe9b
Jonathan Carter <=>
2020-09-20 12:12:02 -0400

    better spaced fucntions and removed un needed attribute from reduce initial value

--- part4/Blog list/utils/list_helper.js
+++ part4/Blog list/utils/list_helper.js
@@ -4,35 +4,40 @@ const dummy = _ => {
 
 const totalLikes = blogs => {
   if(blogs.length === 0) return 0
+
   return blogs.reduce((total, {likes}) => total + +likes, 0)
 }
 
 const favoriteBlog = blogs => {
   if(blogs.length === 0) return {}
+
   const { title, author, likes } = blogs.reduce((favorite, current) => {
     return current.likes > favorite.likes ? current : favorite
   },{ likes:0 })
+
   return { title, author, likes }
 }
 
 const mostBlogs = blogs => {
   if(blogs.length === 0) return {}
+
   const authors = {}
 
   return blogs.reduce((top,{author}) => { 
     authors[author] ? authors[author]++ : authors[author] = 1
     return authors[author] < top.blogs ? top : { author, blogs: authors[author] }
-  }, { author: '', blogs: 0 })
+  }, { blogs: 0 })
 }
 
 const mostLikes = blogs => {
   if(blogs.length === 0) return {}
+  
   const authors = {}
 
   return blogs.reduce((top,{author, likes}) => { 
     authors[author] ? authors[author] += likes : authors[author] = likes
     return authors[author] < top.likes ? top : { author, likes: authors[author] }
-  }, { author: '', likes: 0 })
+  }, { likes: 0 })
 }
 
 module.exports = {
