171a0464564ca91045c4cfd4b29c846625026ecb
Jonathan Carter <graphicnapkin@me.com>
2020-10-05 22:04:10 -0400

    updated login, blog_get create_user and Login component

--- part5/bloglist-frontend/src/components/Login.js
+++ part5/bloglist-frontend/src/components/Login.js
@@ -9,27 +9,29 @@ const Login = props => {
     setPassword
   } = props
   return (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
+    <><h2>Log in to the Application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
   )
 }
 
